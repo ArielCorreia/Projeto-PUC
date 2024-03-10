6129e5a702ee3607aa6d95b4e7a96dae3f37698c
Ariel <162646728+ArielCorreia@users.noreply.github.com>
2024-03-10 16:51:28 -0300

    Controle de estoque

---
+++ APP/config/DBConnection.py
@@ -0,0 +1,26 @@
+from urllib.parse import quote
+
+from sqlalchemy import create_engine, text, ForeignKey
+
+from sqlalchemy.orm import scoped_session, sessionmaker, declarative_base, Session
+
+import cx_Oracle
+
+lib_dir = "D:\Programa de desenvolvimneto gti\ORACLE\instantclient-basic-windows.x64-21.13.0.0.0dbru\instantclient_21_13"
+cx_Oracle.init_oracle_client(lib_dir=lib_dir)
+
+USER = 'CTL_ESTOQUE'
+PASSWD = quote('admin') # quote é utilizado para ajustar a URL que possui caracteres especiais
+HOST = 'localhost'
+PORT = 1521
+SID = "xe"
+
+
+sid = cx_Oracle.makedsn(HOST, PORT, sid=SID)
+instance = f"oracle+cx_oracle://{USER}:{PASSWD}@{sid}"
+engine = create_engine(url=instance, echo=True, max_identifier_length=30)
+session = scoped_session(sessionmaker(bind=engine, autoflush=True,autocommit=False))
+response = session.execute(text('SELECT * FROM MUNICIPIO'))
+for o in response:
+    print(o)
+
