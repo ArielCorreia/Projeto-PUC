277a9fba7089103c0934a176b63e27b67b827dbd
Ariel <162646728+ArielCorreia@users.noreply.github.com>
2024-03-22 20:43:53 -0300

    Update menu_principal.py

--- APP/view/menu_principal.py
+++ APP/view/menu_principal.py
@@ -7,11 +7,11 @@ from view.Atendimento.registro_pedido import *
 from view.Atendimento.consulta_pedido import *
 
 from view.Controle_Estoque.lista_estoque import lista_produtos_estoque
+from view.Controle_Estoque.consultar_produto import consultar_produto_estoque
 
-from view.Gestao.produto.cadastro_produto import *
+from view.Gestao.produto.cadastro_produto import cadastro_produto
 from view.Gestao.produto.lista_produtos import *
-from view.Gestao.cliente.cadastro_cliente import *
-
+from view.Gestao.cliente.cadastro_cliente import cadastro_cliente_PF,cadastro_cliente_PJ
 
 
 from tkinter import *
@@ -20,7 +20,7 @@ from tkinter import *
 #Janela PRINCIPAL = app
 app = Tk()
 app.title("App Controle de Estoque")
-app.geometry("780x350")
+app.geometry("800x500")
 app.configure(background="#dde")
 
 ## Menu ##
@@ -37,6 +37,11 @@ Barra_menu.add_cascade(label="Atendimento",menu=atendimento)
 # Controle de estoque
 controle_est = Menu(Barra_menu, tearoff=0)
 controle_est.add_command(label="Produtos em Estoque",command= lista_produtos_estoque)
+controle_est.add_separator()
+controle_est.add_command(label="Consultar Produto",command= consultar_produto_estoque)
+controle_est.add_command(label="Movimentação do Produto",command= None)
+controle_est.add_separator()
+controle_est.add_command(label="Alterar Locação",command= None)
 Barra_menu.add_cascade(label="Controle de Estoque",menu=controle_est)
 
 # Gestão de produtos 
@@ -59,10 +64,10 @@ Barra_menu.add_cascade(label="Gestão de Clientes",menu=gestao_cliente)
 
 # Cadastros
 cadastros = Menu(Barra_menu, tearoff=0)
-cadastros.add_cascade(label="Cadastro de Pessoa Física",menu=cadastro_cliente)
-cadastros.add_cascade(label="Cadastro de Pessoa Jurídica",menu=None)
+cadastros.add_command(label="Cadastro de Pessoa Física",command=cadastro_cliente_PF)
+cadastros.add_command(label="Cadastro de Pessoa Jurídica",command=cadastro_cliente_PJ)
 cadastros.add_separator()
-cadastros.add_cascade(label="Cadastro de Produto",menu=cadastro_produto)
+cadastros.add_command(label="Cadastro de Produto",command=cadastro_produto)
 Barra_menu.add_cascade(label="Cadastros",menu=cadastros)
 
 # Sistema - Fechar
