73edfffd9f5d1bcb9f64b1681b8963cd4ebe7e20
Ariel <162646728+ArielCorreia@users.noreply.github.com>
2024-04-03 18:54:50 -0300

    Delete APP directory

--- APP/models/base.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.orm import DeclarativeBase
-from config.DBConnection import * 
-
-class Base(DeclarativeBase):
-    @classmethod
-    def add(cls):
-        session.add(cls)
-        session.flush()
