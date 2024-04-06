423202bed11b38cbc14399ccac3b900c19f7da07
Ariel <162646728+ArielCorreia@users.noreply.github.com>
2024-04-06 12:31:59 -0300

    Update pessoa_fisica.py

--- APP/models/pessoa_fisica.py
+++ APP/models/pessoa_fisica.py
@@ -6,8 +6,9 @@ from models.pessoa import Pessoa
 from config.DBConnection import * 
 
 class Pessoa_Fisica (Base):
+class Pessoa_Fisica (Base):
     __tablename__ = 'PESSOA_FISICA'
-    cd_cliente: Mapped[int] = mapped_column(INTEGER, ForeignKey(Pessoa.cd_pessoa), nullable=False, primary_key=True)
+    cd_pessoa_fisica: Mapped[int] = mapped_column(INTEGER, ForeignKey(Pessoa.cd_pessoa), nullable=False, primary_key=True)
     nr_cpf: Mapped[str] = mapped_column(CHAR(11), nullable=False, unique=True) 
     dt_nascimento: Mapped[datetime] = mapped_column(DATE, nullable=False)
     tp_genero: Mapped[str] = mapped_column(CHAR(1), nullable=False)
